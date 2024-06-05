6782c5de6f57fbb342efc20c156e7322aafd684a
chxgbx <yungryce@yahoo.com>
2024-06-05 04:51:52 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 # lockboxes
 
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
+
+
+"""
 def canUnlockAll(boxes):
     x = len(boxes)
     item = [False] * x
@@ -15,3 +32,4 @@ def canUnlockAll(boxes):
                 stack.append(each)
 
     return all(item)
+"""
